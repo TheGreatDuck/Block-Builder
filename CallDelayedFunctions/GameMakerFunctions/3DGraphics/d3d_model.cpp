87bb15f9fa936b5ce6bd5bac468428c73f3502cb
TheGreatDuck <thegreatduckinh@gmail.com>
2020-09-01 02:45:17 -0500

    Game Maker Function Completion Commit 2
    Moved Game Maker 3D model functionality wrapper functions to a designated file.

--- CallDelayedFunctions/GameMakerFunctions/3DGraphics/d3d_model.cpp
+++ CallDelayedFunctions/GameMakerFunctions/3DGraphics/d3d_model.cpp
@@ -2,13 +2,22 @@
 #include <string.h>
 #include "delayed_function_calls.h"
 
-ADD_FUNCTION(d3d_model_draw)
 ADD_FUNCTION(d3d_model_create)
-ADD_FUNCTION(d3d_model_load)
-ADD_FUNCTION(d3d_model_primitive_begin)
-ADD_FUNCTION(d3d_model_vertex_texture)
-ADD_FUNCTION(d3d_model_primitive_end)
 ADD_FUNCTION(d3d_model_destroy)
+ADD_FUNCTION(d3d_model_clear)
+ADD_FUNCTION(d3d_model_save)
+ADD_FUNCTION(d3d_model_load)
+ADD_FUNCTION(d3d_model_draw)
+ADD_FUNCTION(d3d_model_primitive_begin)
+ADD_FUNCTION(d3d_model_vertex)
+ADD_FUNCTION(d3d_model_vertex_color)
+ADD_FUNCTION(d3d_model_vertex_texture)
+ADD_FUNCTION(d3d_model_vertex_texture_color)
+ADD_FUNCTION(d3d_model_vertex_normal)
+ADD_FUNCTION(d3d_model_vertex_normal_color)
+ADD_FUNCTION(d3d_model_vertex_normal_texture)
+ADD_FUNCTION(d3d_model_vertex_normal_texture_color)
+ADD_FUNCTION(d3d_model_primitive_end)
 
 void d3d_model_draw(int* ind, double x, double y, double z, int* texid)
 {
