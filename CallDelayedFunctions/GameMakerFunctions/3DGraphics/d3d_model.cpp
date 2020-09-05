fa8b1b570b68fbfbc79c749e0225cbf57aa67b01
TheGreatDuck <thegreatduckinh@gmail.com>
2020-09-05 01:53:58 -0500

    Game Maker Function Completion Commit 8
    Simplifying code.

--- CallDelayedFunctions/GameMakerFunctions/3DGraphics/d3d_model.cpp
+++ CallDelayedFunctions/GameMakerFunctions/3DGraphics/d3d_model.cpp
@@ -26,47 +26,30 @@ void d3d_model_create(int* ind)
 
 void d3d_model_destroy(double ind)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    addDelayedFunctionCall(FP_d3d_model_destroy, 0, 0, input0);
+    addDelayedFunctionCall(FP_d3d_model_destroy, NULL, 0, ind);
 }
 
 void d3d_model_load(int* ind, const char* fname)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    delayedInput input1 = convertToDelayedInput(fname);
-    addDelayedFunctionCall(FP_d3d_model_load, NULL, 0, input0, input1);
+    addDelayedFunctionCall(FP_d3d_model_load, NULL, 0, ind, fname);
 }
 
 void d3d_model_draw(int* ind, double x, double y, double z, int* texid)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    delayedInput input1 = convertToDelayedInput(x);
-    delayedInput input2 = convertToDelayedInput(y);
-    delayedInput input3 = convertToDelayedInput(z);
-    delayedInput input4 = convertToDelayedInput(texid);
-    addDelayedFunctionCall(FP_d3d_model_draw, NULL, 0, input0, input1, input2, input3, input4);
+    addDelayedFunctionCall(FP_d3d_model_draw, NULL, 0, ind, x, y, z, texid);
 }
 
 void d3d_model_primitive_begin(int* ind, double kind)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    delayedInput input1 = convertToDelayedInput(kind);
-    addDelayedFunctionCall(FP_d3d_model_primitive_begin, 0, 0, input0, input1);
+    addDelayedFunctionCall(FP_d3d_model_primitive_begin, NULL, 0, ind, kind);
 }
 
 void d3d_model_vertex_texture(int* ind, double x, double y, double z, double xtex, double ytex)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    delayedInput input1 = convertToDelayedInput(x);
-    delayedInput input2 = convertToDelayedInput(y);
-    delayedInput input3 = convertToDelayedInput(z);
-    delayedInput input4 = convertToDelayedInput(xtex);
-    delayedInput input5 = convertToDelayedInput(ytex);
-    addDelayedFunctionCall(FP_d3d_model_vertex_texture, 0, 0, input0, input1, input2, input3, input4, input5);
+    addDelayedFunctionCall(FP_d3d_model_vertex_texture, NULL, 0, ind, x, y, z, xtex, ytex);
 }
 
 void d3d_model_primitive_end(int* ind)
 {
-    delayedInput input0 = convertToDelayedInput(ind);
-    addDelayedFunctionCall(FP_d3d_model_primitive_end, 0, 0, input0);
+    addDelayedFunctionCall(FP_d3d_model_primitive_end, NULL, 0, ind);
 }
