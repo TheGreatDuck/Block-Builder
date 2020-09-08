5f96b0499d0b649ecdd80414596dc76fa25148ef
TheGreatDuck <thegreatduckinh@gmail.com>
2020-09-08 14:32:10 -0500

    Update gameLoop.cpp

--- CallDelayedFunctions/gameLoop.cpp
+++ CallDelayedFunctions/gameLoop.cpp
@@ -27,20 +27,20 @@ GMEXPORT double gameLoopInit()
 
     load_level("C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\1025\\data\\Savegame\\7-24-2020 hideout\\Block Builder Checkouting\\Block_Builder_3D\\Block-Builder\\Build Files", "YggdrasilQuest", "1");
 
-    sprite_add("C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\1025\\data\\Savegame\\7-24-2020 hideout\\Block Builder Checkouting\\Block_Builder_3D\\Block-Builder\\Build Files\\block.png", 1, 0, 0, 0, 0, &blkGph->spr_blockTexture);
-    sprite_get_texture(&blkGph->spr_blockTexture,0,&blkGph->tex_blockTexture);
+    //sprite_add("C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\1025\\data\\Savegame\\7-24-2020 hideout\\Block Builder Checkouting\\Block_Builder_3D\\Block-Builder\\Build Files\\block.png", 1, 0, 0, 0, 0, &blkGph->spr_blockTexture);
+    //sprite_get_texture(&blkGph->spr_blockTexture,0,&blkGph->tex_blockTexture);
 
     //sprite_add("C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\1025\\data\\Savegame\\7-24-2020 hideout\\Block Builder Checkouting\\Block_Builder_3D\\Block-Builder\\Build Files\\block.png", 1, 0, 0, 0, 0, &player.texBody);
     //sprite_get_texture(&blkGph->spr_blockTexture,0,&blkGph->tex_blockTexture);
 
     initGameControl();
 
-    d3d_start();
-    d3d_set_fog(1,0,600,1000);
+    //d3d_start();
+    //d3d_set_fog(1,0,600,1000);
     //draw_set_color(c_white);
     //texture_set_interpolation(0);
     //d3d_set_culling(true);
-    d3d_set_lighting(0);
+    //d3d_set_lighting(0);
 
     return 1.0;
 }
